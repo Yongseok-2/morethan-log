d7f3101c302f704a9ce7b5f214c6c3e199bbc78c
Yongseok-2 <8465867@naver.com>
2025-10-05 20:32:08 +0900

    Update Tag.tsx

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,21 +1,36 @@
-import styled from "@emotion/styled"
 import { useRouter } from "next/router"
 import React from "react"
 
 type Props = {
   children: string
+  tag_id: number
 }
 
-const Tag: React.FC<Props> = ({ children }) => {
+const Tag: React.FC<Props> = ({ children, tag_id }) => {
   const router = useRouter()
-
   const handleClick = (value: string) => {
     router.push(`/?tag=${value}`)
   }
+
+  let arrayOfColors = [
+    "bg-red-200",
+    "bg-yellow-200",
+    "bg-green-200",
+    "bg-blue-200",
+    "bg-indigo-200",
+    "bg-purple-200",
+    "bg-pink-200",
+  ]
+
   return (
-    <StyledWrapper onClick={() => handleClick(children)}>
+    <div
+      onClick={() => handleClick(children)}
+      className={`text-xs text-gray-800 font-normal rounded-full ${
+        arrayOfColors[tag_id % arrayOfColors.length]
+      } px-2 py-1 cursor-pointer`}
+    >
       {children}
-    </StyledWrapper>
+    </div>
   )
 }
 
@@ -34,3 +49,4 @@ const StyledWrapper = styled.div`
   background-color: ${({ theme }) => theme.colors.gray5};
   cursor: pointer;
 `
+
