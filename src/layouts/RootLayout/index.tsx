7816d6aaf5910712a890a5a863f9d33b8b19a612
Yongseok-2 <8465867@naver.com>
2025-10-06 17:45:33 +0900

    Update index.tsx

--- src/layouts/RootLayout/index.tsx
+++ src/layouts/RootLayout/index.tsx
@@ -45,11 +45,44 @@ type Props = {
 }
 
 const RootLayout = ({ children }: Props) => {
+  const router = useRouter()
+  const currentElementRef = useRef<HTMLDivElement | null>(null)
+
+  const [blogHeight, setBlogHeight] = useState(0)
+  const [throttleScrollY, setThrottleScrollY] = useState<number>(0)
+  const [scheme] = useScheme()
   const [scheme] = useScheme()
   useGtagEffect()
   useEffect(() => {
     Prism.highlightAll();
   }, []);
+
+    const scrollThrottle = useThrottle(() => {
+    setThrottleScrollY(window.scrollY)
+  }, 100)
+
+  const getCurrentPercentage = () => {
+    if (router.asPath === "/") return 0
+
+    let percentage = Math.ceil((throttleScrollY / blogHeight) * 100)
+
+    if (percentage >= 90) {
+      percentage = 100
+    } else {
+      percentage = Math.ceil((throttleScrollY / blogHeight) * 100)
+    }
+    return percentage
+  }
+
+  useEffect(() => {
+    if (currentElementRef.current) {
+      const clientHeight = currentElementRef.current.clientHeight
+      setBlogHeight(clientHeight)
+    }
+
+    window.addEventListener("scroll", scrollThrottle)
+    return () => window.removeEventListener("scroll", scrollThrottle)
+  }, [throttleScrollY])
 
   return (
     <ThemeProvider scheme={scheme}>
