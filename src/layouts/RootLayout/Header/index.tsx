974433c33d9e5d5f22e7e287896fecbde805af17
Yongseok-2 <8465867@naver.com>
2025-10-06 18:01:39 +0900

    Update index.tsx

--- src/layouts/RootLayout/Header/index.tsx
+++ src/layouts/RootLayout/Header/index.tsx
@@ -6,11 +6,13 @@ import { zIndexes } from "src/styles/zIndexes"
 
 type Props = {
   fullWidth: boolean
+    readingProgress: number
 }
 
 const Header: React.FC<Props> = ({ fullWidth }) => {
   return (
     <StyledWrapper>
+            <HeaderWrapper>
       <div data-full-width={fullWidth} className="container">
         <Logo />
         <div className="nav">
@@ -18,11 +20,41 @@ const Header: React.FC<Props> = ({ fullWidth }) => {
           <NavBar />
         </div>
       </div>
+                    </HeaderWrapper>
+            <ReadingProgress readingProgress={readingProgress} />
     </StyledWrapper>
   )
 }
 
 export default Header
+
+const HeaderWrapper = styled.div`
+  padding: 10px 0;
+  background-color: ${({ theme }) => theme.colors.grayA1};
+  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
+  .container {
+    display: flex;
+    padding-left: 1rem;
+    padding-right: 1rem;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    max-width: 1120px;
+    height: 3rem;
+    margin: 0 auto;
+    &[data-full-width="true"] {
+      @media (min-width: 768px) {
+        padding-left: 6rem;
+        padding-right: 6rem;
+      }
+    }
+    .nav {
+      display: flex;
+      gap: 0.75rem;
+      align-items: center;
+    }
+  }
+`
 
 const StyledWrapper = styled.div`
   z-index: ${zIndexes.header};
