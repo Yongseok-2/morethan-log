ccbd2f07024a7a0fd61645c0039a01b1fac6eb39
Yongseok-2 <8465867@naver.com>
2025-10-13 17:22:11 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   images: {
-    domains: ['ring-vein-852.notion.site', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
+    domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
   },
 }
